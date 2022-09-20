30229d63dc73d93bc6d2c7ca7822036dc3aac684
jpcortesgd <jpcortesg@eafit.edu.co>
2022-09-20 15:44:59 -0500

    create initial pages

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,13 +1,57 @@
+import { BsFillPlayFill, BsInfoCircle } from "react-icons/bs";
 import "./featured.scss";
 
-export default function Featured() {
+export default function Featured({ type }) {
   return (
     <div className="featured">
+      {type && (
+        <div className="category">
+          <span> {type === "movie" ? "Movies" : "Series "} </span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="criem">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-Fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
       <img
         width="100%"
-        src="https://womenssupportnetwork.ca/wp-content/uploads/2021/02/women-homepage.jpg"
+        src="https://cdn.pocket-lint.com/r/s/x1920/assets/images/155659-tv-news-feature-what-is-the-best-order-to-watch-the-matrix-movies-image6-n4msmyjaxw.jpg"
         alt="profile picture"
       />
+      <div className="info">
+        <img
+          src="https://occ-0-2433-1432.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABeeMILAyu161f6TSuh-poQWNKfIo_pl9aeifrQ7noJsTYX78AbLCLDsPzCHSp7N0OfCGrU3nGDFtynNnZ3lKQbXuPgpbRObPUCcAPFgwtp2K.png?r=df1"
+          alt=""
+        />
+        <span className="desc">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam
+          officiis et eligendi, quasi architecto, nihil nostrum debitis ullam
+          necessitatibus eum nesciunt, quaerat aliquam. Aperiam corporis in
+          illum aut, vero earum.
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <BsFillPlayFill />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <BsInfoCircle />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/list/List.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useRef } from "react";
+import { HiOutlineChevronLeft, HiOutlineChevronRight } from "react-icons/hi";
+import ListItem from "../listItem/ListItem";
+import "./list.scss";
+
+export default function List() {
+  const listRef = useRef();
+  const [sliderNumber, setSliderNumber] = useState(0);
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    let size = listRef.current.children.length;
+    if (direction === "left" && sliderNumber > 0) {
+      setSliderNumber((current) => current - 1);
+      listRef.current.style.transform = `translateX(${distance + 230}px)`;
+    }
+    if (direction === "right" && sliderNumber < size - 6) {
+      setSliderNumber((current) => current + 1);
+      listRef.current.style.transform = `translateX(${distance - 230}px)`;
+    }
+  };
+
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <HiOutlineChevronLeft
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <HiOutlineChevronRight
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+}
